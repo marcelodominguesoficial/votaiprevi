a3b03fd08aae42437d99dacf0baaeee3e5e1718f
Marcelo Domingues <mdomingues53@hotmail.com>
2022-06-21 11:32:47 -0300

    Ajustes na tela de eleicao e ajuste na imagem de fundo da home

--- src/main/java/br/com/votaaiprevi/VotaaipreviApplication.java
+++ src/main/java/br/com/votaaiprevi/VotaaipreviApplication.java
@@ -6,13 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VotaaipreviApplication {
 
-	/*
-	@Bean
-    public Java8TimeDialect java8TimeDialect() {
-        return new Java8TimeDialect();
-    }
-    */
-	
 	public static void main(String[] args) {
 		SpringApplication.run(VotaaipreviApplication.class, args);
 	}
